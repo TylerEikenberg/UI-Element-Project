b3bb3426c93996cb13ba8f461ad3e1d66c3b02ba
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-18 11:34:54 -0400

    added event listener for character tab

--- script.js
+++ script.js
@@ -19,26 +19,33 @@ showPanel(0, '#8406a1');
 /**
  * **************************************************************************
  */
+/**
+ * Noah helped me get connected to the API
+ */
 
 const marvelKeyPublic = '4be43da26f4e8d17b4c2804ae80c48d3';
 const marvelKeyPrivate = '1e512f1ad0b7af9586a731e12e07db918d04d8fb';
-
+//getAuthUrl gets the authorized url with given API keys
 function getAuthUrl() {
   let timestamp = Date.now();
   let hash = md5(timestamp + marvelKeyPrivate + marvelKeyPublic);
   let auth = `?&ts=${timestamp}&apikey=${marvelKeyPublic}&hash=${hash}`;
   return auth;
 }
-// set charactersUrl to url of all Marvel characters
-const baseUrl = 'https://gateway.marvel.com/v1/public';
-const charactersUrl = 'https://gateway.marvel.com:443/v1/public/characters' + getAuthUrl();
 
-fetch(charactersUrl)
-  .then(res => res.json())
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => console.log(err));
+const baseUrl = 'https://gateway.marvel.com/v1/public' + getAuthUrl(); // set baseUrl to main marvel info
+const charactersUrl = 'https://gateway.marvel.com:443/v1/public/characters' + getAuthUrl(); // set charactersUrl to url of all Marvel characters
+
+const charactersTabButton = document.querySelector('.marvelCharacters');
+charactersTabButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  fetch(charactersUrl)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err));
+});
 
 // fetch(url, {
 //     headers: {
