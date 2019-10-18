df5959eb4c197ad71fddb2f7d4be3616fb1b4183
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-10-18 11:29:49 -0400

    got marvel api working

--- script.js
+++ script.js
@@ -16,6 +16,42 @@ function showPanel(panelIndex, colorCode) {
 }
 
 showPanel(0, '#8406a1');
+/**
+ * **************************************************************************
+ */
 
-// marvel api public key 4be43da26f4e8d17b4c2804ae80c48d3
-// marvel api private key 1e512f1ad0b7af9586a731e12e07db918d04d8fb
+const marvelKeyPublic = '4be43da26f4e8d17b4c2804ae80c48d3';
+const marvelKeyPrivate = '1e512f1ad0b7af9586a731e12e07db918d04d8fb';
+
+function getAuthUrl() {
+  let timestamp = Date.now();
+  let hash = md5(timestamp + marvelKeyPrivate + marvelKeyPublic);
+  let auth = `?&ts=${timestamp}&apikey=${marvelKeyPublic}&hash=${hash}`;
+  return auth;
+}
+// set charactersUrl to url of all Marvel characters
+const baseUrl = 'https://gateway.marvel.com/v1/public';
+const charactersUrl = 'https://gateway.marvel.com:443/v1/public/characters' + getAuthUrl();
+
+fetch(charactersUrl)
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => console.log(err));
+
+// fetch(url, {
+//     headers: {
+//       'cat-api-key': '947a2018-feec-4a13-a15d-081376ec218a',
+//     },
+//   })
+//     .then(res => res.json())
+//     .then(res => {
+//       catImage = res[0].url;
+//       console.log(res);
+//       console.log(catImage);
+//       img.src = catImage;
+//     })
+
+//     .catch(err => console.log(err));
+// });
